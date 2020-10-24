840d7907229b22257281343bda9eaca139f0c411
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2020-10-24 14:22:17 +0700

    update

--- themeConfig.js
+++ themeConfig.js
@@ -1,6 +1,6 @@
 // MAIN COLORS - VUESAX THEME COLORS
 let colors = {
-	primary: '#7367F0',
+	primary: '#7367f0',
 	success: '#28C76F',
 	danger: '#EA5455',
 	warning: '#FF9F43',
@@ -14,15 +14,15 @@ Vue.use(Vuesax, { theme:{ colors } });
 
 // CONFIGS
 const themeConfig = {
-	theme: 'Dark',						// options[String]: 'light'(default), 'dark', 'semi-dark'
+	theme: 'dark',						// options[String]: 'light'(default), 'dark', 'semi-dark'
 	sidebarCollapsed: false,			// options[Boolean]: true, false(default)
 	navbarColor: "#fff",				// options[String]: HEX color / rgb / rgba / Valid HTML Color name - (default: #fff)
 	navbarType: "floating",				// options[String]: floating(default) / static / sticky / hidden
 	footerType: "static",				// options[String]: static(default) / sticky / hidden
 	routerTransition: 'zoom-fade',		// options[String]: zoom-fade / slide-fade / fade-bottom / fade / zoom-out / none(default)
-	disableCustomizer: false,			// options[Boolean]: true, false(default)
+	disableCustomizer: true,			// options[Boolean]: true, false(default)
 	hideScrollToTop: false,				// options[Boolean]: true, false(default)
-	disableThemeTour: false,					// options[Boolean]: true, false(default)
+	disableThemeTour: true,					// options[Boolean]: true, false(default)
   // NOTE: themeTour will be disabled in screens < 1200. Please refer docs for more info.
 }
 
